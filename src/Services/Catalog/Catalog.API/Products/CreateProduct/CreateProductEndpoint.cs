09fff1cacb62a33f645697cdbf89f6017eb7de0b
Emircan Duman <emircandum10@gmail.com>
2025-10-22 15:56:31 +0300

    CQRS Library

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+
+    public record CreateProductRequest(string Name, List<string> category, string Description, string ImageFile, decimal Price);
+
+    public record CreateProductResponse(Guid ProductId);
+    public class CreateProductEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product entity from incoming command object
+            
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+
+            };
+
+            // save to database
+
+            // return createproductresult with new product id
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
