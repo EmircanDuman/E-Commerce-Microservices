a27bffdc2eb0cd1aeb33de0d1f859dc60ac99a7f
Emircan Duman <emircandum10@gmail.com>
2025-10-22 19:25:36 +0300

    Implement post handlers

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -4,7 +4,22 @@
     public record CreateProductRequest(string Name, List<string> category, string Description, string ImageFile, decimal Price);
 
     public record CreateProductResponse(Guid ProductId);
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async(CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.ProductId}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create product")
+            .WithDescription("Creates a new product.");
+        }
     }
 }
