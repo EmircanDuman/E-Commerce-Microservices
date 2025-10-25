45c1e82b57f16f78bc593362346b7cab571aecca
Emircan Duman <emircandum10@gmail.com>
2025-10-25 15:37:16 +0300

    Behaviors and Dockerization for Catalog.API

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,7 +2,9 @@
 global using Mapster;
 global using MediatR;
 global using Marten;
+global using Catalog.API.Data;
 global using FluentValidation;
+global using Marten.Pagination;
 global using Catalog.API.Models;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Exceptions;
