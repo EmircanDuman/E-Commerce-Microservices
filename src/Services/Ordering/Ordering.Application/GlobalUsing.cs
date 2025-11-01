42a35a16c6739422a28e778460ff05290a32022f
Emircan Duman <emircandum10@gmail.com>
2025-11-01 16:11:15 +0300

    Queries and Event handlers

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,7 +1,13 @@
-﻿global using FluentValidation;
+﻿global using MediatR;
+global using FluentValidation;
 global using BuildingBlocks.CQRS;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
+global using BuildingBlocks.Pagination;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Data;
+global using Microsoft.Extensions.Logging;
 global using Ordering.Domain.ValueObjects;
+global using Microsoft.EntityFrameworkCore;
 global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
